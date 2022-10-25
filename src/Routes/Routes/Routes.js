4dca7d8b5015d64bf075910cf41f02c35b9038e2
Puja Dey <pujadey48@gmail.com>
2022-10-25 16:07:38 +0600

    create layout and some components

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main";
+import Home from "../../Pages/Home/Home";
 
 
  export const  routes = createBrowserRouter([
@@ -7,7 +8,10 @@ import Main from "../../layout/Main";
         path: '/',
         element: <Main></Main>,
         children: [
-            
+            {
+                path: '/',
+                element:<Home></Home>
+            }
         ]
     }
  ])
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
+import Header from '../Pages/Shared/Header/Header';
+import LeftSideNav from '../Pages/Shared/LeftSideNav/LeftSideNav';
 
 const Main = () => {
     return (
         <div>
-            <Outlet></Outlet>
+            <Header></Header>
+
+            <Container>
+                <Row>
+                    <Col lg="4">
+                        <LeftSideNav></LeftSideNav>
+                    </Col>
+                    <Col lg = "8">
+                        <Outlet></Outlet>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
