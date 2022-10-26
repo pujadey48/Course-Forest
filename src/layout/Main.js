3bf91f5defefcb587f56149f2e0ae952435f1f0f
Puja Dey <pujadey48@gmail.com>
2022-10-26 20:25:19 +0600

    some imports optimized

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 import Header from '../Pages/Shared/Header/Header';
-import LeftSideNav from '../Pages/Courses/LeftSideNav/LeftSideNav';
 
 const Main = () => {
     return (
